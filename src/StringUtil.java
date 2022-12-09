02579fdb2700e7a016aeba215cf2f7f8204d4933
jakobsym <jakob.symchych@my.utsa.edu>
2022-12-09 15:14:48 -0600

    Class that converts user input into SHA-256 hash

---
+++ src/StringUtil.java
@@ -0,0 +1,27 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class StringUtil {
+    /* Applies SHA256 to a string and returns its result */
+    public static String applySha256(String input){
+        // code may have exceptions so using try and catch blocks
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            // Applying SHA-256 onto user input
+            byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8)); // Turns input into SHA-256 hash
+
+            StringBuffer hexString = new StringBuffer(); // Using StringBuffer class, as its more secure than StringBuilder
+
+            for(int i = 0; i < hash.length; ++i){
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
