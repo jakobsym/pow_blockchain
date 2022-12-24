8387b47fbf39106079ecf460f099ecb60d395d5c
jakobsym <jakob.symchych@my.utsa.edu>
2022-12-24 09:01:54 -0700

    created blockchain package, added Wallet class

--- src/Block.java
+++ src/Block.java
@@ -6,7 +6,7 @@ public class Block {
     public String previousHash;
     private String data; // unique transaction or some data, will be using a message in this example
     private long timeStamp; // number milliseconds since genesis block
-    private int nonce;
+    private int nonce; // nonce is an arbitrary pseudo-random number
 
 
     //Block Constructor
@@ -22,13 +22,12 @@ public class Block {
     public String calculateHash(){
         return StringUtil.applySha256(previousHash + timeStamp + nonce + data);
     }
-
-
-
+    
     /* public void mineBlock(int difficulty)
         - create new char array of size difficulty, which is turned into a string
         - This string is filled with 0's which must be solved for as proof of work
         - I.E: difficulty = 5, target = '00000'
+        - Miner must
 
         - This method will trigger on e/a new block
         - This method will trigger when checking if Blocks are valid(solved)
