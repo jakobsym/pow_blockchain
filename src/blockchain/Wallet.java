e5cc0755c8787b4dab5a46213f4a468278d95059
jakobsym <jakob.symchych@my.utsa.edu>
2022-12-29 10:02:00 -0700

    begin creating wallet, added public/private keys generation

--- src/blockchain/Wallet.java
+++ src/blockchain/Wallet.java
@@ -1,5 +1,34 @@
 package blockchain;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
 
+    public PrivateKey privateKey; // Public address (displayed on-chain)
+    public PublicKey publicKey;  // private address (user keeps secret)
+
+    // Generate keys using Elliptic-curve cryptography via 'Java.security.KeyPairGenerator'
+
+    public Wallet(){
+        generateKeyPair();
+    }
+    public void generateKeyPair() {
+        try{
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+
+            // generate keyPair after init of key generator
+            keyGen.initialize(ecSpec, random); //256 byte as security
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            // assign public/private generated keys
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
